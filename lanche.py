dbfc0245e031fbe5661f4a06a30d3e7bbe8996ce
rodriguesvicenzzo0-ship-it <rodriguesvicenzzo0@gmail.com>
2026-03-30 12:49:58 -0300

    Add files via upload

---
+++ lanche.py
@@ -0,0 +1,10 @@
+valor = float(input("qual o valor do lanche?"))
+
+if valor <= 10.00:
+    print("preço justo")
+elif valor <= 20.00:
+    print("está começando a ficar caro")
+else:
+    print("está muito caro, é melhor trazer de casa")
+
+  
